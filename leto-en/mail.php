7eb80329751b6d0f86ef4c4a36e7aa22547ffbb0
VanderveldeKR <vanderveldekr@gmail.com>
2023-10-23 14:28:30 +0500

    leto site

---
+++ leto-en/mail.php
@@ -0,0 +1,23 @@
+<?php
+    $name = $_POST['name'];
+	$phone = $_POST['phone'];
+    $cheese = $_POST['cheese'];
+
+	$to = ""; 
+	$date = date ("d.m.Y"); 
+	$time = date ("h:i");
+	$subject = "Заказ Звонка";
+
+	
+	$msg="
+    Имя: $name;
+    Телефон: $phone;
+    Согласие на обработку: $cheese";
+    
+	mail($to, $subject, $msg, "From: $to ");
+
+?>
+
+    <div class="header__form-text pop-up__body-text end-form">
+        <h2 class="title-style">Successfully sent</h2>
+    </div>
